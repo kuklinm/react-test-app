ff93c0b4dc430e9df98d09bf1c273edb764aedf1
kuklinm <zolozavr@gmail.com>
2018-05-20 16:10:57 +0300

    First commit

---
+++ src/conponents/header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="logo">
+                    <div className="logo">Logo</div>
+                    <input type="text"/>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
